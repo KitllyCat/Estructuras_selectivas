8ee0fa1929a88c172892ce0001b5541854584248
KitllyCat <cjchambem@unjbg.edu.pe>
2024-06-16 21:27:59 -0500

    Actualizando y ordenando ejercicio 03

--- Ejercicio_03.cpp
+++ Ejercicio_03.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 int main() {
+	
     int numCamisas;
     float precioUnitario, totalmonto, descuento, pagomonto;
+    
 	descuento = 0;
+	totalmonto = numCamisas * precioUnitario;
+	
     cout << "Ingresa el numero de camisas compradas: "<<endl;
     cin >> numCamisas;
     cout << "Ingresa el precio unitario por camisa: "<<endl;
     cin >> precioUnitario;
-
-    totalmonto = numCamisas * precioUnitario;
 
     if (numCamisas >= 3) {
         descuento = (20 * totalmonto) / 100;
@@ -20,9 +22,9 @@ int main() {
 
     pagomonto = totalmonto - descuento;
 
-    cout << "El monto total a pagar: S/ " << totalmonto << endl;
-    cout << "Eñ descuento aplicado: S/ " << descuento << endl;
-    cout << "Y el monto a pagar despues del descuento: S/ " << pagomonto << endl;
+    cout << "El monto total a pagar: S/ " << totalmonto << " y el descuento aplicado es de: S/"<<descuento<< endl;
+    cout << "Entonces, el monto a pagar despues del descuento: S/ " << pagomonto << endl;
     return 0;
+    
 }
 
