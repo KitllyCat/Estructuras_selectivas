2ed5107f1a0a0f142fb8e0d5b35474a724495281
KitllyCat <cjchambem@unjbg.edu.pe>
2024-06-17 22:38:20 -0500

    Actualizando y ordenando el ejercicio 04

--- Ejercicio_04.cpp
+++ Ejercicio_04.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 int main() {
+	
     int montoTotal, descuento, numeroEscogido;
 
-    cout << "Ingresa el monto total de la compra: ";
+    cout << "Ingresa el monto total de la compra: "<<endl;
     cin >> montoTotal;
-    cout << "Ingresa el numero escogido al azar: ";
+    cout << "Ingresa el numero escogido al azar: "<<endl;
     cin >> numeroEscogido;
 	descuento = 0;
-    if (numeroEscogido < 74) {
+	
+    if (numeroEscogido =< 74) {
         descuento = (15 * montoTotal) / 100;
     } else {
         descuento = (20 * montoTotal) / 100;
     }
 
-    cout << "El descuento aplicado es de: S/ " << descuento << endl;
+    cout << "El descuento aplicado con el numero "<<numeroEscogido<<" es de: S/ " << descuento << endl;
 
     return 0;
+    
 }
 
